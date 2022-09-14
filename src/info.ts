292d0eec41cd60ca8951a384888f5c1ce0fb48b2
Morilence <1280659615@qq.com>
2022-09-14 23:39:25 +0800

    fix: relaxed MatterInfo for data inclusion

--- src/info.ts
+++ src/info.ts
@@ -1,8 +1,8 @@
 export interface MatterInfo {
-    created: Date;
-    updated: Date;
-    author: string;
-    committer: string;
-    contributors: string[];
+    created?: Date;
+    updated?: Date;
+    author?: string;
+    committer?: string;
+    contributors?: string[];
     [key: string]: any;
 }
