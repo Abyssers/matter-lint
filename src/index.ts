d85639a98f37473b80703ddccafa71366078d6b4
Morilence <1280659615@qq.com>
2022-09-14 20:38:49 +0800

    perf: made the execution structure clearer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import { resolve } from "node:path";
 import { readFileSync } from "node:fs";
 import { jit } from "@abysser/jit";
+import { read } from "gray-matter";
+import { MatterInfo } from "./info";
+import { MatterOption } from "./option";
+import { lint } from "./lint";
 
 const cwd: string = process.cwd();
 const args: string[] = process.argv.slice(2);
+
+/* default configurations */
+lint.add("config", true, resolve(cwd, ".matterlint.json"), (opt: MatterOption, data: MatterInfo) => {
+    return;
+});
+
+lint.add("write", false, true, (opt: MatterOption, data: MatterInfo) => {
+    if (!opt.value) {
+        /* log something */
+    } else {
+        /* write back */
+    }
+});
+
+const { opts, paths } = lint.parse(cwd, args);
