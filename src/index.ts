6d5007a175f09f4c690d3128df44d787b97d6de8
Morilence <1280659615@qq.com>
2022-09-16 01:33:20 +0800

    feat: added --now option

--- src/index.ts
+++ src/index.ts
@@ -2,99 +2,109 @@ import { isAbsolute, extname, resolve } from "node:path";
 import { existsSync, readFileSync, writeFileSync } from "node:fs";
 import { stringify } from "gray-matter";
 import { jit } from "@abysser/jit";
-import { MatterInfo } from "./info";
 import { MatterOption } from "./option";
-import { lint } from "./lint";
+import { MatterLintContext, lint } from "./lint";
 
 const cwd: string = process.cwd();
 const args: string[] = process.argv.slice(2);
 
 /* default configurations */
-lint.add(
-    "config",
-    true,
-    resolve(cwd, ".matterlint.json"),
-    (opt: MatterOption, _path: string, data: MatterInfo, content: string) => {
-        if (
-            typeof opt.value === "string" &&
-            isAbsolute(opt.value) &&
-            extname(opt.value) === ".json" &&
-            existsSync(opt.value)
-        ) {
-            const opts = JSON.parse(readFileSync(opt.value, { encoding: "utf8" }));
+lint.add("config", true, resolve(cwd, ".matterlint.json"), (opt: MatterOption) => {
+    if (
+        typeof opt.value === "string" &&
+        isAbsolute(opt.value) &&
+        extname(opt.value) === ".json" &&
+        existsSync(opt.value)
+    ) {
+        const opts = JSON.parse(readFileSync(opt.value, { encoding: "utf8" }));
 
-            Object.keys(opts).forEach(key => {
-                const option = lint.get(key);
-                if (option && option.name !== "config") {
-                    lint.config(option.name, opts[key]);
-                }
-            });
-        }
-        return [data, content];
+        Object.keys(opts).forEach(key => {
+            const option = lint.get(key);
+            if (option && option.name !== "config") {
+                lint.config(option.name, opts[key]);
+            }
+        });
     }
-);
-
-lint.add("force", false, false, (opt: MatterOption, path: string, data: MatterInfo, content: string) => {
-    if (typeof opt.value === "boolean") {
-        const commits = jit.repo(cwd).do("log", ["--pretty=fuller", "--", "<path>"], path).formatted as any[];
-        if (opt.value) {
-            data["author"] = commits[commits.length - 1]["author"]["name"] as string;
-            data["committer"] = commits[0]["author"]["name"] as string;
-            data["contributors"] = [...new Set(commits.map(commit => commit["author"]["name"]))].join(",") as string;
-            data["created"] = commits[commits.length - 1]["authorDate"] as Date;
-            data["updated"] = new Date();
-        } else {
-            const { hasOwnProperty: has } = Object.prototype;
-            !has.call(data, "author") && (data["author"] = commits[commits.length - 1]["author"]["name"] as string);
-            !has.call(data, "committer") && (data["committer"] = commits[0]["author"]["name"] as string);
-            !has.call(data, "contributors") &&
-                (data["contributors"] = [...new Set(commits.map(commit => commit["author"]["name"]))].join(
-                    ","
-                ) as string);
-            !has.call(data, "created") && (data["created"] = commits[commits.length - 1]["authorDate"] as Date);
-            !has.call(data, "updated") && (data["updated"] = new Date());
-        }
-    }
-    return [data, content];
 });
 
-lint.add("map", true, "", (opt: MatterOption, _path: string, data: MatterInfo, content: string) => {
+lint.add("force", false, false, (opt: MatterOption, ctx: MatterLintContext) => {
+    if (typeof opt.value === "boolean") {
+        const { path } = ctx;
+        const commits = jit.repo(cwd).do("log", ["--pretty=fuller", "--", "<path>"], path).formatted as any[];
+        if (opt.value) {
+            ctx.data["author"] = commits[commits.length - 1]["author"]["name"] as string;
+            ctx.data["committer"] = commits[0]["author"]["name"] as string;
+            ctx.data["contributors"] = [...new Set(commits.map(commit => commit["author"]["name"]))].join(
+                ","
+            ) as string;
+            ctx.data["created"] = commits[commits.length - 1]["authorDate"] as Date;
+            ctx.data["updated"] = commits[0]["authorDate"] as Date;
+        } else {
+            const { hasOwnProperty: has } = Object.prototype;
+            !has.call(ctx.data, "author") &&
+                (ctx.data["author"] = commits[commits.length - 1]["author"]["name"] as string);
+            !has.call(ctx.data, "committer") && (ctx.data["committer"] = commits[0]["author"]["name"] as string);
+            !has.call(ctx.data, "contributors") &&
+                (ctx.data["contributors"] = [...new Set(commits.map(commit => commit["author"]["name"]))].join(
+                    ","
+                ) as string);
+            !has.call(ctx.data, "created") && (ctx.data["created"] = commits[commits.length - 1]["authorDate"] as Date);
+            !has.call(ctx.data, "updated") && (ctx.data["updated"] = commits[0]["authorDate"] as Date);
+        }
+        ctx.commits = commits;
+        ctx.force = opt.value;
+    }
+});
+
+lint.add("now", false, false, (opt: MatterOption, ctx: MatterLintContext) => {
+    if (typeof opt.value === "boolean") {
+        if (opt.value) {
+            const { force } = ctx;
+            const now: Date = new Date();
+            if (force) {
+                now > ctx.data["updated"] && (ctx.data["updated"] = now);
+            } else {
+                const { hasOwnProperty: has } = Object.prototype;
+                !has.call(ctx.data, "updated") && now > ctx.data["updated"] && (ctx.data["updated"] = now);
+            }
+        }
+    }
+});
+
+lint.add("map", true, "", (opt: MatterOption, ctx: MatterLintContext) => {
     if (typeof opt.value === "string") {
         opt.value
             .split(";")
             .map(p => p.split(":"))
             .forEach(map => {
                 const [originField, mappedField] = map;
-                if (Object.keys(data).includes(originField)) {
-                    data[mappedField] = data[originField];
-                    delete data[originField];
+                if (Object.keys(ctx.data).includes(originField)) {
+                    ctx.data[mappedField] = ctx.data[originField];
+                    delete ctx.data[originField];
                 }
             });
     }
-    return [data, content];
 });
 
-lint.add("blank-lines", true, "1", (opt: MatterOption, _path: string, data: MatterInfo, content: string) => {
+lint.add("blank-lines", true, "1", (opt: MatterOption, ctx: MatterLintContext) => {
     if (typeof opt.value === "string" && !Number.isNaN(Number(opt.value))) {
         let rn = false;
-        while (content.startsWith("\r\n") || content.startsWith("\n")) {
-            if (content.startsWith("\r\n")) (content = content.slice(2)) && (rn = true);
-            if (content.startsWith("\n")) content = content.slice(1);
+        while (ctx.content.startsWith("\r\n") || ctx.content.startsWith("\n")) {
+            if (ctx.content.startsWith("\r\n")) (ctx.content = ctx.content.slice(2)) && (rn = true);
+            if (ctx.content.startsWith("\n")) ctx.content = ctx.content.slice(1);
         }
-        content = new Array(Math.ceil(Number(opt.value))).fill(rn ? "\r\n" : "\n").join("") + content;
+        ctx.content = new Array(Math.ceil(Number(opt.value))).fill(rn ? "\r\n" : "\n").join("") + ctx.content;
     }
-    return [data, content];
 });
 
-lint.add("write", false, false, (opt: MatterOption, path: string, data: MatterInfo, content: string) => {
+lint.add("write", false, false, (opt: MatterOption, ctx: MatterLintContext) => {
     if (typeof opt.value === "boolean") {
         if (opt.value) {
-            writeFileSync(path, stringify(content, data));
+            writeFileSync(ctx.path, stringify(ctx.content, ctx.data));
         } else {
-            console.log(data);
+            console.log(ctx.data);
         }
     }
-    return [data, content];
 });
 
 lint.run(cwd, args);
