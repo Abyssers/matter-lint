25b4b944557ecaef08854c9f1d08887fcb4737ea
Morilence <1280659615@qq.com>
2022-09-22 00:12:27 +0800

    fix: changed --force to apply created only

--- src/index.ts
+++ src/index.ts
@@ -41,27 +41,18 @@ lint.add("force", false, false, (opt: MatterOption, ctx: MatterLintContext) => {
         const latestCommit = commits[0];
         const earliestCommit = commits[commits.length - 1];
         const authors = jit.repo(cwd).do("shortlog", ["-sne", "HEAD", "--", "<path>"], path).formatted;
+        ctx.data["author"] = earliestCommit["author"]["name"] as string;
+        ctx.data["contributors"] = (authors as MatterInfo["contributors"]).map(author => {
+            author["contributions"] = author["summary"];
+            delete author["summary"];
+            return author;
+        });
+        ctx.data["updatedby"] = latestCommit["author"]["name"] as string;
+        ctx.data["updated"] = latestCommit["authorDate"] as Date;
         if (opt.value) {
-            ctx.data["author"] = earliestCommit["author"]["name"] as string;
-            ctx.data["contributors"] = (authors as MatterInfo["contributors"]).map(author => {
-                author["contributions"] = author["summary"];
-                delete author["summary"];
-                return author;
-            });
-            ctx.data["updatedby"] = latestCommit["author"]["name"] as string;
-            ctx.data["updated"] = latestCommit["authorDate"] as Date;
             ctx.data["created"] = earliestCommit["authorDate"] as Date;
         } else {
             const { hasOwnProperty: has } = Object.prototype;
-            !has.call(ctx.data, "author") && (ctx.data["author"] = earliestCommit["author"]["name"] as string);
-            !has.call(ctx.data, "contributors") &&
-                (ctx.data["contributors"] = (authors as MatterInfo["contributors"]).map(author => {
-                    author["contributions"] = author["summary"];
-                    delete author["summary"];
-                    return author;
-                }));
-            !has.call(ctx.data, "updatedby") && (ctx.data["updatedby"] = latestCommit["author"]["name"] as string);
-            !has.call(ctx.data, "updated") && (ctx.data["updated"] = latestCommit["authorDate"] as Date);
             !has.call(ctx.data, "created") && (ctx.data["created"] = earliestCommit["authorDate"] as Date);
         }
         ctx.force = opt.value;
@@ -93,7 +84,9 @@ lint.add("map", true, "", (opt: MatterOption, ctx: MatterLintContext) => {
             .forEach(map => {
                 const [originField, mappedField] = map;
                 if (Object.keys(ctx.data).includes(originField)) {
-                    ctx.data[mappedField] = ctx.data[originField];
+                    if (!(!ctx.force && originField === "created" && ctx.data[mappedField])) {
+                        ctx.data[mappedField] = ctx.data[originField];
+                    }
                     delete ctx.data[originField];
                 }
             });
